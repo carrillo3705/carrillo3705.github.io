bb996afa888547d8364195ed40df3b0556c2aefb
carrillo3705 <43218150+carrillo3705@users.noreply.github.com>
2018-10-23 18:53:07 -0500

    Add files via upload

--- baseConverter.js
+++ baseConverter.js
@@ -1,45 +1,43 @@
-function tenToSmall(num, base) //convert a base-10 number to a smaller base
+function tenToSmall(num, base)
 {
-    // this function is expecting a number (num) in base 10
-    // it will convert the number (num) to value of 'base'
-    num = (document.smallToTen.input1.value); // number to convert
-    base = 2; // destination base
-    smallNum =""; // store remainders here
 
-    while ( num > 0) // continue looping while num is not zero
+    var num = Number (document.toBinary.result.value) ;
+    base = 2;
+    smallNum ="";
+
+    while ( num > 0)
     {
-        smallNum = num%base + smallNum; //3%5 = 2... $ return remainders
-        //setup up for the iteration of the loop
+        smallNum = num%base + smallNum;
+
         num = Math.floor(num/2);
         alert(smallNum);
         alert (num);
 
     }
     return smallNum;
-
 }
-function smallToTen(num, base) //convert a small-base number to base-10
+
+function smallToTen(num, base)
 {
-    num = (document.tenToSmall.input1.value); //13
-    base = 2; //base pf 'num' to convert to base 10
-    tenNum = 0; //stores number to be output at the end
-    numLength = num.toString().length; // converts num to STRING, then counts the number of characters in the string
-    // in this case, length of '1101' is 4
+    var num = Number (document.toBaseTen.result.value);
+    base = 2;
+    tenNum = 0;
+    numLength = num.toString().length;
 
-    while (numLength > 0) {
-        var denominator = Math.pow(10, (numLength - 1)); // gets denominator of fraction
-        // for 1101, denominators will be 1000, 100, 10, 1
-        var currentDigit = Math.floor(num / denominator);
-        // above means 1101/1000 = 1.... 101/100 = 1..... 01/10 = 0
-        tenNum = tenNum + currentDigit * Math.pow(base, numLength - 1)
-        // currentDigit*2^3..... currentDigit*2^2
-        num = num % denominator; // reduces num for the next iteration
+    while (numLength > 0)
+    {
+      var denominator = Math.pow(10,(numLength-1));
 
-        numLength--; // decrease the numLength by 1... otherwise will loop infinitely
+      var currentDigit = Math.floor(num/denominator);
+
+        tenNum = tenNum + currentDigit*Math.pow(base, numLength-1)
+
+        num = num%denominator;
+
+        numLength--;
 
     }
 
     return tenNum;
-
-
 }
+
