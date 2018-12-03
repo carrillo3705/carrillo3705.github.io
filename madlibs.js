6fcbf7142af705b90f4fa79b5e5ea674415f68ce
carrillo3705 <43218150+carrillo3705@users.noreply.github.com>
2018-12-03 11:43:45 -0600

    Add files via upload

---
+++ madlibs.js
@@ -0,0 +1,38 @@
+
+
+function wordPush() {
+
+    var madlibs = []
+    var word1 = (document.words.word1.value);
+    var word2 = (document.words.word2.value);
+    var word3 = (document.words.word3.value);
+    var word4 = (document.words.word4.value);
+    var word5 = (document.words.word5.value);
+    var word6 = (document.words.word6.value);
+    var word7 = (document.words.word8.value);
+    var word8 = (document.words.word8.value);
+    var word9 = (document.words.word9.value);
+    var word10 = (document.words.word10.value);
+
+
+    madlibs.push(word1);
+    madlibs.push(word2);
+    madlibs.push(word3);
+    madlibs.push(word4);
+    madlibs.push(word5);
+    madlibs.push(word6);
+    madlibs.push(word7);
+    madlibs.push(word8);
+    madlibs.push(word9);
+    madlibs.push(word10);
+
+
+    alert("The year 2218 Guy Fieri continues to his journey to eat every food to ever exist ever, eating many things along the way especially" + " " + madlibs[0] + "." + " He has been eating the most" + " " + madlibs[1] + ", " + madlibs[2] + "and " + madlibs[3] + " " + "of foods." + " Yet there were foods that still alluded him that he could not reach in hidden lands because the guards made of" + " " + madlibs[4] + "." + " To sneak past the guards made of " + madlibs[4] + " he created a disguise made of " + madlibs[5] + "." + " His movements were " + madlibs[6] + "and" + madlibs[7] + " to successfully get past the guards." + " Finally, his dream was within reach getting closer to the goal he has been pursuing for his whole life. Under a beam of light and on on a golden pedestal was a meal so " + madlibs[8] + "because it was made out of "
+        + madlibs[9] + ".")
+}
+
+
+
+
+
+
